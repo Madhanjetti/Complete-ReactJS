194655e06ed7b364a094563a197ce331517b7f2f
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2024-07-23 10:00:17 +0530

    Update MapReactjs.js

--- MapReactjs.js
+++ MapReactjs.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 function App() {
-  const arr=["reactJs","ExpressJs","NodeJs","MOndodb","Javascript"]
+  const arr=["reactJs","ExpressJs","NodeJs","Mongodb","Javascript"]
   return (
     <div>
     {
